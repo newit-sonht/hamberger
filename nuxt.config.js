015e59fbe6bf2e42391803335c7edc7415304676
Newit-sonht <91664001+newit-sonht@users.noreply.github.com>
2022-09-06 10:52:50 +0700

    Core Base

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/core-plugins.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,5 +39,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: null,
   }
 }
